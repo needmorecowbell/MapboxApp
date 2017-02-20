8c8da349a5aec31781fba1c4f49f091d05aca11e
Adam Musciano <amusciano@gmail.com>
2017-02-20 16:52:37 -0500

    Changed layout and added clustering

--- app/src/main/java/com/adammusciano/mapboxmap/MainActivity.java
+++ app/src/main/java/com/adammusciano/mapboxmap/MainActivity.java
@@ -1,12 +1,28 @@
 package com.adammusciano.mapboxmap;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.mapbox.mapboxsdk.MapboxAccountManager;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.style.layers.CircleLayer;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonOptions;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static com.mapbox.mapboxsdk.style.layers.Filter.all;
+import static com.mapbox.mapboxsdk.style.layers.Filter.gte;
+import static com.mapbox.mapboxsdk.style.layers.Filter.lt;
+import static com.mapbox.mapboxsdk.style.layers.Filter.neq;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.circleBlur;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.circleColor;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.circleRadius;
 
 public class MainActivity extends AppCompatActivity {
     private MapView mapView;
@@ -23,11 +39,69 @@ public class MainActivity extends AppCompatActivity {
         mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(MapboxMap mapboxMap) {
-
+                addClusteredGeoJsonSource(mapboxMap);
                 // Interact with the map using mapboxMap here
 
             }
         });
+    }
+
+    private void addClusteredGeoJsonSource(MapboxMap mapboxMap) {
+
+        // Add a new source from our GeoJSON data and set the 'cluster' option to true.
+        try {
+            mapboxMap.addSource(
+                    // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes from
+                    // 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
+                    new GeoJsonSource("earthquakes",
+                            new URL("https://www.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson"),
+                            new GeoJsonOptions()
+                                    .withCluster(true)
+                                    .withClusterMaxZoom(15) // Max zoom to cluster points on
+                                    .withClusterRadius(20) // Use small cluster radius for the heatmap look
+                    )
+            );
+        } catch (MalformedURLException malformedUrlException) {
+            Log.e("heatmapActivity", "Check the URL " + malformedUrlException.getMessage());
+        }
+
+        // Use the earthquakes source to create four layers:
+        // three for each cluster category, and one for unclustered points
+
+        // Each point range gets a different fill color.
+        final int[][] layers = new int[][]{
+                new int[]{15, Color.parseColor("#E55E5E")},
+                new int[]{5, Color.parseColor("#F9886C")},
+                new int[]{0, Color.parseColor("#FBB03B")}
+        };
+
+        CircleLayer unclustered = new CircleLayer("unclustered-points", "earthquakes");
+        unclustered.setProperties(
+                circleColor(Color.parseColor("#FBB03B")),
+                circleRadius(20f),
+                circleBlur(1f));
+        unclustered.setFilter(
+                neq("cluster", true)
+        );
+        mapboxMap.addLayer(unclustered, "building");
+
+        for (int i = 0; i < layers.length; i++) {
+
+            CircleLayer circles = new CircleLayer("cluster-" + i, "earthquakes");
+            circles.setProperties(
+                    circleColor(layers[i][1]),
+                    circleRadius(70f),
+                    circleBlur(1f)
+            );
+            circles.setFilter(
+                    i == 0
+                            ? gte("point_count", layers[i][0]) :
+                            all(gte("point_count", layers[i][0]), lt("point_count", layers[i - 1][0]))
+            );
+            mapboxMap.addLayer(circles, "building");
+        }
+
+
     }
 
     @Override
